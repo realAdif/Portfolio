92768b8fe9eeecaf3705e1a6f1762fce1310462f
Aditya <adityagaikwad693@gmail.com>
2022-09-27 22:43:18 +1000

    Fixed Background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
-import "./styles/App.css"
+// import "./styles/App.css"
 
 function App(){
     return(
