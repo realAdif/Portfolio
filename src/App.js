c18742d142c5fde4b91096a507908385f639ca3a
Aditya <adityagaikwad693@gmail.com>
2022-10-04 01:42:18 +1100

    Updated the Website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React,{useEffect} from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 
 
 function App(){
+    // This effect runs once, after the first render
+    useEffect(() => {
+        document.title = "Aditya PortFolio"
+    }, [])
     return(
         <div>
+            
             <PortfolioContainer />
         </div>
     )
